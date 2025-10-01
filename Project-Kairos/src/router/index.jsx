047fb31c86b57b0cb1800b2b18b71ad7353f6117
ThiagoRAlmeida2 <thiagoralmeida23@gmail.com>
2025-10-01 09:49:23 -0300

    teste

--- Project-Kairos/src/router/index.jsx
+++ Project-Kairos/src/router/index.jsx
@@ -3,10 +3,10 @@ import { createBrowserRouter } from "react-router-dom";
 // Por enquanto só rota inicial
 const router = createBrowserRouter([
   {
-    
     path: "/",
     element: <div className="text-center mt-10 text-2xl font-bold">Hello World 🚀</div>,
   },
 ]);
+
 
 export default router;
