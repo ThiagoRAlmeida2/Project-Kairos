f60928c78b068c57488b82aa2a9ba9f7f81c6ca3
ThiagoRAlmeida2 <thiagoralmeida23@gmail.com>
2025-10-01 09:44:41 -0300

    configuração ok

--- Project-Kairos/src/router/index.jsx
+++ Project-Kairos/src/router/index.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 // Por enquanto só rota inicial
 const router = createBrowserRouter([
   {
+    
     path: "/",
     element: <div className="text-center mt-10 text-2xl font-bold">Hello World 🚀</div>,
   },
