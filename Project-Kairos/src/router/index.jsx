d03aa3cde21903be5f75644c3939b6282ca2023d
ThiagoRAlmeida2 <thiagoralmeida23@gmail.com>
2025-10-01 14:37:31 -0300

    ajuste de router

--- Project-Kairos/src/router/index.jsx
+++ Project-Kairos/src/router/index.jsx
@@ -1,11 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-import CadastroCard from "../components/CadastroCard"; // ajuste o caminho se necessário
 
 // Rota inicial exibindo o card de cadastro
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <CadastroCard />,
   },
 ]);
 
