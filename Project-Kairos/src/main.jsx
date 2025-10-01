3a795e086e013227e7c73e8b954c10f9533c1325
ThiagoRAlmeida2 <thiagoralmeida23@gmail.com>
2025-09-30 22:54:17 -0300

    Projeto configurado

--- Project-Kairos/src/main.jsx
+++ Project-Kairos/src/main.jsx
@@ -1,8 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
-import "./Css/index.css";
-
+import "./css/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
