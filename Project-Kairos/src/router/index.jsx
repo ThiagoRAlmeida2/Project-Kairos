e2266d8a7601952ff3577bb8eae2f5e98f902e32
mateustavares-bit <paozin270@gmail.com>
2025-10-07 11:28:29 -0300

    Update index.jsx
    Alteração do index para rota inicial e card de cadastro

--- Project-Kairos/src/router/index.jsx
+++ Project-Kairos/src/router/index.jsx
@@ -1,5 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+import CadastroCard from "../components/CadastroCard"; 
 
+// Por enquanto só rota inicial
+// Rota inicial exibindo o card de cadastro
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CadastroCard />,
+  },
+]);
+
+
+export default router;
 // Rota inicial exibindo o card de cadastro
 const router = createBrowserRouter([
   {
