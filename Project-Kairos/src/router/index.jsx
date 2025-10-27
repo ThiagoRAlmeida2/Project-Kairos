851b626390b9081d36a2a6c2fc420e0ea17beed1
Menes <pereiramouton@gmail.com>
2025-10-27 11:24:01 -0300

    Remove node_modules e adiciona componentes de eventos

--- Project-Kairos/src/router/index.jsx
+++ Project-Kairos/src/router/index.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../pages/App.jsx";
 import Home from "../pages/Home.jsx";
 import ProjetosList from "../pages/projeto.jsx";
+import Eventos from "../pages/eventos.jsx";
 
 export default createBrowserRouter([
   {
@@ -11,6 +12,7 @@ export default createBrowserRouter([
     children: [
       { index: true, element: <Home /> },          // "/"
       { path: "projetos", element: <ProjetosList /> }, // "/projetos"
+      { path: "eventos", element: <Eventos />}
     ],
   },
 ]);
