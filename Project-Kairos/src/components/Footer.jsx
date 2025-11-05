234f823c8c37c9d9a2840bd728b7b9090025d004
ThiagoRAlmeida2 <thiagoralmeida23@gmail.com>
2025-11-05 17:21:27 -0300

    ajuste footer

--- Project-Kairos/src/components/Footer.jsx
+++ Project-Kairos/src/components/Footer.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
+import footerBanner from '../assets/IMG/Banner-rodapé.png'; 
+
 export default function Footer() {
   return (
     <footer className="footer" id="contato">
       <div className="footer__banner">
-        <img src="/src/assets/IMG/Banner-rodapé.png" alt="Banner rodapé" className="footer__banner-image" />
+        
+        {/* 2. USAR A VARIÁVEL IMPORTADA NO 'src' */}
+        <img 
+          src={footerBanner} // <-- Usando a variável importada
+          alt="Banner rodapé" 
+          className="footer__banner-image" 
+        />
+        
         <div className="footer__banner-content">
           <div className="container footer__inner">
             <p>© {new Date().getFullYear()} Kairos. Todos os direitos reservados.</p>
