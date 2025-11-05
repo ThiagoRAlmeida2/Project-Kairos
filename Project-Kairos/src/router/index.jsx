112a008d3b66d58ea8c3de499193e8cb48b91439
ThiagoRAlmeida2 <thiagoralmeida23@gmail.com>
2025-11-05 10:59:02 -0300

    ajuste de router

--- Project-Kairos/src/router/index.jsx
+++ Project-Kairos/src/router/index.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import App from "../pages/App.jsx";
 import Home from "../pages/Home.jsx";
+
+// 🚩 CORREÇÃO: Usando o nome do arquivo/componente consistente
 import ProjetosList from "../pages/projeto.jsx"; 
 import Eventos from "../pages/eventos.jsx";
 import Perfil from "../pages/perfil.jsx";
@@ -13,10 +15,10 @@ export default createBrowserRouter([
   {
     element: <App />, 
     children: [
-      { index: true, element: <Home /> },              // "/"
-      { path: "projetos", element: <ProjetosList /> }, // "/projetos"
-      { path: "eventos", element: <Eventos /> },       // "/eventos"
-      { path: "perfil", element: <Perfil /> },         // "/perfil"
+      { index: true, element: <Home /> },                  // "/"
+      { path: "projetos", element: <ProjetosList /> },      // "/projetos"
+      { path: "eventos", element: <Eventos /> },           // "/eventos"
+      { path: "perfil", element: <Perfil /> },             // "/perfil"
       { path: "dashboard", element: <EmpresaDashboard /> } // "/dashboard"
     ],
   },
