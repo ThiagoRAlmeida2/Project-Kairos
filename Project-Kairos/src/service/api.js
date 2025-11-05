be7b47a16a52993206773de696125463311dd023
ThiagoRAlmeida2 <thiagoralmeida23@gmail.com>
2025-11-05 11:11:13 -0300

    ajuste da conexão da api

--- Project-Kairos/src/service/api.js
+++ Project-Kairos/src/service/api.js
@@ -1,17 +1,17 @@
 // api.js
 import axios from 'axios';
 
-// Base URL da API
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8081';
+
 const api = axios.create({
-    baseURL: 'http://localhost:8081', // porta do seu Spring Boot
+    baseURL: baseURL,
     headers: {
         'Content-Type': 'application/json'
     }
 });
 
-// Interceptor para adicionar token JWT automaticamente
 api.interceptors.request.use(config => {
-    const token = localStorage.getItem('jwtToken'); // ou onde você armazenar
+    const token = localStorage.getItem('token');
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
