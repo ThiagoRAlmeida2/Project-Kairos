a5bdc0c8e3a0b9f19eb2ef9bb2e2bc0f1af58f78
ThiagoRAlmeida2 <thiagoralmeida23@gmail.com>
2025-11-15 11:57:41 -0300

    ajuste de api

--- Work-up/src/service/api.js
+++ Work-up/src/service/api.js
@@ -6,16 +6,16 @@ const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8081';
 const api = axios.create({
     baseURL: baseURL
 });
-
 api.interceptors.request.use(config => {
         const token = localStorage.getItem('token'); 
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
-    if (!(config.data instanceof FormData)) {
+    if (config.data instanceof FormData) {
+        delete config.headers['Content-Type'];
+    } else {
         config.headers['Content-Type'] = 'application/json';
     }
-    
     return config;
 });
 
